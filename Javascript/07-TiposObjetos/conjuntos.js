143ef3b22a1e6d9314645177867118122b51db8d
Danijfox <danijfox@gmail.com>
2022-09-14 14:07:47 +0200

    #	new file:   Javascript/07-TiposObjetos/conjuntos.js #

---
+++ Javascript/07-TiposObjetos/conjuntos.js
@@ -0,0 +1,10 @@
+const miFamilia = ["Florentina", "Antonio", "Toño"];
+
+const nombresFamilia = new Set(miFamilia);
+
+console.log(nombresFamilia);
+nombresFamilia.add("Daniel");
+
+console.log(nombresFamilia);
+
+nombresFamilia.add("Javascript");
