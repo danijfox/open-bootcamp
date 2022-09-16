ae0abafaa876c5596260e7a42ff6fab361e64e26
Danijfox <danijfox@gmail.com>
2022-09-16 16:51:35 +0200

    #	new file:   Javascript/08-Funciones/errores.js #

---
+++ Javascript/08-Funciones/errores.js
@@ -0,0 +1,34 @@
+//gestión de errores
+
+const dobla = (val) => {
+  if (typeof val === "number") {
+    return 2 * val;
+  } else {
+    //return false
+    throw new Error("El valor debe ser de tipo número");
+  }
+};
+
+const elDoble = dobla(3);
+
+/// Cuando tenemos un código en un punto donde puede fallar, se usa try catch para envolver el codigo
+
+const numero = "cuatrp";
+try {
+  // codigo que puede fallar
+  const doble = dobla(numero);
+  console.log("Correcto" + doble);
+} catch (e) {
+  console.log("error");
+  console.error(e);
+  // si falla ejecuta lo que hay aquí
+} finally {
+  // si ocurre algún error o no
+}
+
+//Errores comunes:
+// Internal error
+// Syntax error
+// Range error
+// Reference error
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Error
