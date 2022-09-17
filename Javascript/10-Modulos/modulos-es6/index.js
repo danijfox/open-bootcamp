18edc98d285d5b09ff9533247ae1ccb6bcbdee53
Danijfox <danijfox@gmail.com>
2022-09-17 20:21:55 +0200

    #	modified:   Javascript/10-Modulos/modulos-es6/index.js #	new file:   Javascript/10-Modulos/modulos-es6/modulos/literatura.js #

--- Javascript/10-Modulos/modulos-es6/index.js
+++ Javascript/10-Modulos/modulos-es6/index.js
@@ -1,8 +1,14 @@
-//import { suma, pi } from "./modulos/matematicas.js";
-import * as operaciones from "./modulos/matematicas.js";
-const sum = operaciones.suma(4, 3);
-
-//const sum = suma(4, 3);
+import { suma, pi } from "./modulos/matematicas.js";
+import getAuthor, { libro } from "./modulos/literatura.js";
+//import * as operaciones from "./modulos/matematicas.js";
+//const sum = operaciones.suma(4, 3);
 //console.log(sum);
 
-//console.log(pi);
+const sum = suma(4, 3);
+console.log(sum);
+
+console.log(pi);
+
+const autor = getAuthor();
+console.log(autor);
+console.log(libro);
---
+++ Javascript/10-Modulos/modulos-es6/modulos/literatura.js
@@ -0,0 +1,6 @@
+const getAuthor = () => {
+  return "Miguel de Cervantes";
+};
+export const libro = "Don Quixote de La Mancha";
+
+export default getAuthor;
