b324d8e63d97d5493e422d79a66453de6f5455ad
Danijfox <danijfox@gmail.com>
2022-09-17 20:51:07 +0200

    #	modified:   Javascript/10-Modulos/video-librerias/index.js #	modified:   Javascript/10-Modulos/video-librerias/package.json #

--- Javascript/10-Modulos/video-librerias/index.js
+++ Javascript/10-Modulos/video-librerias/index.js
@@ -1,1 +1,12 @@
-// instalar axios
+// instalar axios npm i axios
+
+import axios from "axios";
+
+axios
+  .get("https://pokeapi.co/api/v2/pokemon/pepe")
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
