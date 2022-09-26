47d2939b218dc379a3ea8fe3a430f0dc6a87f2f6
Danijfox <danijfox@gmail.com>
2022-09-26 18:26:30 +0200

    #	modified:   Javascript/15-Persistencia/ejercicio/index.js #

--- Javascript/15-Persistencia/ejercicio/index.js
+++ Javascript/15-Persistencia/ejercicio/index.js
@@ -6,4 +6,4 @@ var persona = { nombre, apellidos };
 sessionStorage.setItem("persona", JSON.stringify(persona));
 localStorage.setItem("persona", JSON.stringify(persona));
 
-document.cookie = "nombreCookie = caducidadCookie; max-age=3600";
+document.cookie = "nombreCookie = caducidadCookie; max-age=180";
